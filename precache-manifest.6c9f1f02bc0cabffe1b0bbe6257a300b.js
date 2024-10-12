9a100468147d0cb40b08c1e45cdf6f265fe33ead
PravarshaErodula6 <suryasanjeevapravarsha.erodula@slu.edu>
2024-10-11 21:48:07 -0500

    Updates

---
+++ precache-manifest.6c9f1f02bc0cabffe1b0bbe6257a300b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d92dcb39148eb09fe2514e34bb13ba",
+    "url": "/serverless-security/index.html"
+  },
+  {
+    "revision": "ebe41da4e6d61ba601fb",
+    "url": "/serverless-security/static/css/main.2da5288d.chunk.css"
+  },
+  {
+    "revision": "f18e9280047d2c2f67b5",
+    "url": "/serverless-security/static/js/2.afef58d2.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/serverless-security/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "ebe41da4e6d61ba601fb",
+    "url": "/serverless-security/static/js/main.c15ed67b.chunk.js"
+  },
+  {
+    "revision": "a70055d436a5fe4d90c7",
+    "url": "/serverless-security/static/js/runtime~main.7d1edc16.js"
+  },
+  {
+    "revision": "3d775fa7983f91bd4e2f20f4c28ddc37",
+    "url": "/serverless-security/static/media/Pravarsha.3d775fa7.png"
+  },
+  {
+    "revision": "9f602f6e5ae8b0e15d29a8066c271b45",
+    "url": "/serverless-security/static/media/SaiKumar.9f602f6e.png"
+  },
+  {
+    "revision": "2475e907c6cdf5a5362132ebb7340340",
+    "url": "/serverless-security/static/media/Sowjanya.2475e907.png"
+  },
+  {
+    "revision": "747f79b32f46cbdf11f083af9ea7b15d",
+    "url": "/serverless-security/static/media/Sowmya.747f79b3.png"
+  },
+  {
+    "revision": "f18d7b1e2c8ffc01d50c54b9cb90152e",
+    "url": "/serverless-security/static/media/vivek.f18d7b1e.png"
+  }
+]);
